ffdeb1bef598abe4696ca51711e902f450a59ad3
Edveika <edvinasbureika@gmail.com>
2023-10-20 22:56:14 +0300

    Update weather data button functionality added

--- Src/gui.py
+++ Src/gui.py
@@ -6,11 +6,33 @@ from WeatherManager import WeatherManager
 import os
 
 class GUIManager:
-    def __init__(self):
+    def __init__(self, weather_manager):
+        self.weather_manager = weather_manager
+        self.api_manager = weather_manager.get_weather_api()
+
         builder = Gtk.Builder()
         builder.add_from_file("Py-Weather-GUI.glade")
+
         window = builder.get_object("main_window")
         window.show_all()
+
+        self.temp_label = builder.get_object("current_temperature")
+        self.windspeed_label = builder.get_object("current_windspeed")
+        self.cloudcover_label = builder.get_object("current_cloudcover")
+        self.rain_label = builder.get_object("current_rain")
+        self.showers_label = builder.get_object("current_showers")
+        self.snow_label = builder.get_object("current_snow")
+
+        refresh_button = builder.get_object("weather_refresh")
+        refresh_button.connect("clicked", self.refresh_weather_data)
+
+
         Gtk.main()
 
-gui=GUIManager()
+    def refresh_weather_data(self, buttton):
+        self.temp_label.set_text("Current temperature: " + str(self.api_manager.get_cur_temperature()))
+        self.windspeed_label.set_text("Current wind speed: " + str(self.api_manager.get_cur_windspeed()))
+        self.cloudcover_label.set_text("Current cloud cover: " + str(self.api_manager.get_cur_cloudcover()))
+        self.rain_label.set_text("Current rain: " + str(self.api_manager.get_cur_rain()))
+        self.showers_label.set_text("Current showers: " + str(self.api_manager.get_cur_showers()))
+        self.snow_label.set_text("Current snow: " + str(self.api_manager.get_cur_snowfall()))
--- Src/main.py
+++ Src/main.py
@@ -1,16 +1,20 @@
 from WeatherAPI import WeatherAPI
 from WeatherManager import WeatherManager
+from gui import GUIManager
 import threading
 
 def main():
     # Creation of weather manager obj
     weather_manager = WeatherManager()
 
+    weather_manager.set_city("Kaunas")
+
     # Weather Manager thread that is responsable for updating weather information
     wm_thread = threading.Thread(target=weather_manager.update_weather)
     wm_thread.start()
 
-    # GUI will run here
+    # Creation of GUI manager obj
+    gui_manager = GUIManager(weather_manager)
 
 
 main()
