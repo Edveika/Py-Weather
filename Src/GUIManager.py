c4d6ce3b529a937113a26de5c9682adf3ce2432b
Edveika <edvinasbureika@gmail.com>
2023-10-21 15:33:07 +0300

    City picker on startup added

--- Src/GUIManager.py
+++ Src/GUIManager.py
@@ -2,6 +2,7 @@ import gi
 # If multiple versions of GTK installed, make sure 3.0 is installed as well
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from WeatherAPI import WeatherAPI, Status
 from WeatherManager import WeatherManager
 import os
 
@@ -10,32 +11,46 @@ class GUIManager:
         self.weather_manager = weather_manager
         self.api_manager = weather_manager.get_weather_api()
 
-        builder = Gtk.Builder()
-        builder.add_from_file("Py-Weather-GUI.glade")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("Py-Weather-GUI.glade")
 
-        window = builder.get_object("main_window")
-        window.show_all()
+        self.city_input_window()
 
-        self.temp_label = builder.get_object("current_temperature")
-        self.windspeed_label = builder.get_object("current_windspeed")
-        self.cloudcover_label = builder.get_object("current_cloudcover")
-        self.rain_label = builder.get_object("current_rain")
-        self.showers_label = builder.get_object("current_showers")
-        self.snow_label = builder.get_object("current_snow")
+    def city_input_window(self):
+        input_window = self.builder.get_object("city_input_window")
+        input_window.show_all()
 
-        refresh_button = builder.get_object("weather_refresh")
-        refresh_button.connect("clicked", self.refresh_weather_data)
+        input_box = self.builder.get_object("city_name_input")
 
+        weather_manager = self.weather_manager
+        def set_city_from_input(self):
+            selected_city = str(Gtk.Entry.get_text(input_box))
+            set_city_status = weather_manager.set_city(selected_city)
+            if set_city_status == Status.SUCCESS:
+                input_window.destroy()
+                Gtk.main_quit()
+                
+        apply_city_btn = self.builder.get_object("apply_city")
+        apply_city_btn.connect("clicked", set_city_from_input)
 
         Gtk.main()
 
-    def city_input_window(self):
-        pass
-
     def main_window(self):
-        pass
+        window = self.builder.get_object("main_window")
+        window.show_all()
 
-    # TODO: add units
+        self.temp_label = self.builder.get_object("current_temperature")
+        self.windspeed_label = self.builder.get_object("current_windspeed")
+        self.cloudcover_label = self.builder.get_object("current_cloudcover")
+        self.rain_label = self.builder.get_object("current_rain")
+        self.showers_label = self.builder.get_object("current_showers")
+        self.snow_label = self.builder.get_object("current_snow")
+
+        refresh_button = self.builder.get_object("weather_refresh")
+        refresh_button.connect("clicked", self.refresh_weather_data)
+
+        Gtk.main()
+
     def refresh_weather_data(self, buttton):
         units = self.api_manager.get_current_units()
 
--- Src/main.py
+++ Src/main.py
@@ -7,14 +7,19 @@ def main():
     # Creation of weather manager obj
     weather_manager = WeatherManager()
 
-    weather_manager.set_city("Kaunas")
-
     # Weather Manager thread that is responsable for updating weather information
     wm_thread = threading.Thread(target=weather_manager.update_weather)
     wm_thread.start()
 
     # Creation of GUI manager obj
     gui_manager = GUIManager(weather_manager)
+    gui_manager.main_window()
+
+    # set exit flag to stop weather manager when window closed
+    
+
+    # Wait for the weather manager to close and exit the program
+    wm_thread.join()
 
 
 main()
