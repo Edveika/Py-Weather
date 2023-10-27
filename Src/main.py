49c7793cf823650742038d35279e2d97328b0335
Edveika <edvinasbureika@gmail.com>
2023-10-27 18:38:12 +0300

    City set error check
    If user did not input a city, the app will exit

--- Src/main.py
+++ Src/main.py
@@ -13,6 +13,14 @@ def main():
 
     # Creation of GUI manager object
     gui_manager = GUIManager(weather_manager)
+    # If the city was not set(user closed app or something)
+    if not weather_manager.city_is_set():
+        # Cleanup and close the program
+        weather_manager.set_exit_flag()
+        wm_thread.join()
+        exit()
+
+    # Starts the GUI
     gui_manager.main_window()
 
     # Set exit flag to stop weather manager when main window is closed, exit the app
